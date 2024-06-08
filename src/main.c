d801fe672cf437b12026f4a16f4ca4525e84fb52
FireBibi <bibifire.dev@gmail.com>
2024-06-08 16:48:18 +0200

    Removed unused functions

--- src/main.c
+++ src/main.c
@@ -30,18 +30,6 @@ int kdealloc_wrapper(uint64_t addr, size_t size) {
     return ENOTSUP;
 }
 
-int kcall_wrapper(uint64_t func, size_t argc, const uint64_t *argv, uint64_t *ret) {
-    return ENOTSUP;
-}
-
-int physread_wrapper(uint64_t from, void *to, size_t len, uint8_t granule) {
-    return ENOTSUP;
-}
-
-int physwrite_wrapper(void *from, uint64_t to, size_t len, uint8_t granule) {
-    return ENOTSUP;
-}
-
 __attribute__((used)) krw_plugin_initializer_t krw_initializer(krw_handlers_t handlers) {
     requestKernRw();
     
@@ -50,14 +38,5 @@ __attribute__((used)) krw_plugin_initializer_t krw_initializer(krw_handlers_t ha
     handlers->kwrite = kwrite_wrapper;
     handlers->kmalloc = kmalloc_wrapper;
     handlers->kdealloc = kdealloc_wrapper;
-    return 0;
-}
-
-__attribute__((used)) krw_plugin_initializer_t kcall_initializer(krw_handlers_t handlers) {
-    
-    handlers->kcall = kcall_wrapper;
-    handlers->physread = physread_wrapper;
-    handlers->physwrite = physwrite_wrapper;
-
     return 0;
 }
